be9a7b786782810eaea4ce0a2584343505ac5c7e
chskela <chskela@gmail.com>
2022-07-02 20:29:17 +0300

    Update Color.kt - add BlueAlpha600, OnBlueAlpha600Light, OnBlueAlpha600Dark

--- app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.chskela.pomodorotimer.ui.theme
+package com.chskela.pomodorotimer.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueAlpha600 = Color(0x9E4CACFF)
+val OnBlueAlpha600Light = Color(0xFF153047)
+val OnBlueAlpha600Dark = Color(0xFFF2F9FF)
