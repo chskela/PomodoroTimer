be3ba53ef8cfc4bf8ef3b1ac214d3fe19810a2db
chskela <chskela@gmail.com>
2022-07-06 00:34:15 +0300

    Added HSpaser.kt

---
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/components/spaser/HSpaser.kt
@@ -0,0 +1,13 @@
+package com.chskela.pomodorotimer.presentation.ui.components.spaser
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HSpaser(modifier: Modifier = Modifier, height: Dp = 32.dp) {
+    Spacer(modifier = modifier.height(height))
+}
