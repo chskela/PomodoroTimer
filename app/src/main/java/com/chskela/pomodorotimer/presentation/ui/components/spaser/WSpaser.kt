a89c5fd907844721fc6ace9b956a90e9a3ee098f
chskela <chskela@gmail.com>
2022-07-06 00:34:27 +0300

    Added WSpaser.kt

---
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/components/spaser/WSpaser.kt
@@ -0,0 +1,14 @@
+package com.chskela.pomodorotimer.presentation.ui.components.spaser
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WSpaser(modifier: Modifier = Modifier, width: Dp = 16.dp) {
+    Spacer(modifier = modifier.width(width))
+}
