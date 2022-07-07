4b6d110208ae896ea4fbb5c8794833f72db8effe
chskela <chskela@gmail.com>
2022-07-07 13:33:14 +0300

    Added Constants.kt

---
+++ app/src/main/java/com/chskela/pomodorotimer/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.chskela.pomodorotimer.util
+
+object Constants {
+    const val WORKING_PERIOD = 1000 * 60 * 25L
+    const val SHORT_BREAK = 1000 * 60 * 5L
+    const val LONG_BREAK = 1000 * 60 * 15L
+    const val ONE_SECOND = 1000L
+}
