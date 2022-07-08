13b44b0b1080b47d178473f612e967cf13ced8df
chskela <chskela@gmail.com>
2022-07-08 22:12:18 +0300

    Added MainScreen.kt - change Column to ConstraintLayout

--- app/src/main/java/com/chskela/pomodorotimer/presentation/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/screens/main/MainScreen.kt
@@ -1,9 +1,7 @@
 package com.chskela.pomodorotimer.presentation.ui.screens.main
 
 import android.content.res.Configuration
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,32 +10,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.chskela.pomodorotimer.R
 import com.chskela.pomodorotimer.presentation.ui.components.button.UIButton
 import com.chskela.pomodorotimer.presentation.ui.components.button.UIButtonType
 import com.chskela.pomodorotimer.presentation.ui.components.chip.UIChip
-import com.chskela.pomodorotimer.presentation.ui.components.spaser.HSpaser
 import com.chskela.pomodorotimer.presentation.ui.components.spaser.WSpaser
 import com.chskela.pomodorotimer.presentation.ui.theme.PomodoroTimerTheme
 import com.chskela.pomodorotimer.util.UiText
 
 @Composable
 fun MainScreen(mainScreenViewModel: MainScreenViewModel) {
-    Log.e("RESULT", "MainScreen: ${mainScreenViewModel.seconds.value}", )
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
     ) {
-        HSpaser()
-        UIChip()
+        val (buttons, text, chip) = createRefs()
+        UIChip(modifier = Modifier.constrainAs(chip) {
+            top.linkTo(parent.top, margin = 16.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
 
-        Text(modifier = Modifier,
+        Text(
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(chip.bottom)
+                bottom.linkTo(buttons.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
             text = "${mainScreenViewModel.minutes.value}\n${mainScreenViewModel.seconds.value}",
             style = MaterialTheme.typography.titleLarge
         )
 
         Row(
+            modifier = Modifier.constrainAs(buttons) {
+                bottom.linkTo(parent.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -47,13 +58,16 @@ fun MainScreen(mainScreenViewModel: MainScreenViewModel) {
                 description = UiText.StringResource(R.string.dots_three)
             )
             WSpaser()
-            UIButton(type = UIButtonType.Large, onClick = {mainScreenViewModel.startWorkingPeriod()})
+            UIButton(
+                type = UIButtonType.Large,
+                onClick = { mainScreenViewModel.startWorkingPeriod() })
             WSpaser()
             UIButton(
                 icon = R.drawable.fast_forward,
                 backgroundColor = MaterialTheme.colorScheme.secondary,
                 description = UiText.StringResource(R.string.fast_forward)
             )
+
         }
     }
 }
