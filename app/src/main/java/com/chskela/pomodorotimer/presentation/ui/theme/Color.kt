b47fd272bc90b592ed163b0c7c21390bb5d06fff
chskela <chskela@gmail.com>
2022-07-14 19:29:41 +0300

    Fixed Theme.kt - fix BlueAlpha100

--- app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
@@ -6,7 +6,7 @@ val Blue50 = Color(0xFFF2F9FF)
 val Blue900 = Color(0xFF153047)
 val Blue950 = Color(0xFF04090D)
 
-val BlueAlpha100 = Color(0xFFB3E5FC)
+val BlueAlpha100 = Color(0x26B3E5FC)
 val BlueAlpha600 = Color(0x9E4CACFF)
 
 val OnBlueAlpha600Light = Color(0xFF153047)
