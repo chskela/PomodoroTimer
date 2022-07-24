a821113107f5142df3522809c773547365ddf9d7
chskela <chskela@gmail.com>
2022-07-24 16:28:14 +0300

    Update MainActivity.kt - remove PomodoroTimerTheme

--- app/src/main/java/com/chskela/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/chskela/pomodorotimer/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.chskela.pomodorotimer.presentation.PomodoroTimerApp
-import com.chskela.pomodorotimer.presentation.ui.theme.PomodoroTimerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PomodoroTimerTheme {
-               PomodoroTimerApp()
-            }
+            PomodoroTimerApp()
         }
     }
 }
