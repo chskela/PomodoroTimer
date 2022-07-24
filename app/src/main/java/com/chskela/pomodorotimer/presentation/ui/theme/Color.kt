ecbea983ae938ed728187621f12205f6e0ba173a
chskela <chskela@gmail.com>
2022-07-24 16:27:08 +0300

    Update Color.kt - add Green and Red colors

--- app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.chskela.pomodorotimer.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Blue
 val Blue50 = Color(0xFFF2F9FF)
 val Blue900 = Color(0xFF153047)
 val Blue950 = Color(0xFF04090D)
@@ -9,5 +10,18 @@ val Blue950 = Color(0xFF04090D)
 val BlueAlpha100 = Color(0x26B3E5FC)
 val BlueAlpha600 = Color(0x9E4CACFF)
 
-val OnBlueAlpha600Light = Color(0xFF153047)
-val OnBlueAlpha600Dark = Color(0xFFF2F9FF)
+// Green
+val Green50 = Color(0xFFF2FFF5)
+val Green900 = Color(0xFF14401D)
+val Green950 = Color(0xFF040D06)
+
+val GreenAlpha100 = Color(0x264DDA6E)
+val GreenAlpha600 = Color(0x9E4DDA6E)
+
+// Red
+val Red50 = Color(0xFFFFF2F2)
+val Red900 = Color(0xFF471515)
+val Red950 = Color(0xFF0D0404)
+
+val RedAlpha100 = Color(0x26FF4C4C)
+val RedAlpha700 = Color(0xB5FF4C4C)
