7c1278ca6860df0ce587873a3e569996193dccf3
chskela <chskela@gmail.com>
2022-08-27 17:18:18 +0300

    Update MainScreenEvent.kt - add OnReset

--- app/src/main/java/com/chskela/pomodorotimer/presentation/ui/screens/main/MainScreenEvent.kt
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/screens/main/MainScreenEvent.kt
@@ -3,4 +3,5 @@ package com.chskela.pomodorotimer.presentation.ui.screens.main
 sealed class MainScreenEvent{
     object OnStart : MainScreenEvent()
     object OnPause: MainScreenEvent()
+    object OnReset: MainScreenEvent()
 }
