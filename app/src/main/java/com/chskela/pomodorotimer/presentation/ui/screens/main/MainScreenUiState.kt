9aa4bb800d10ea4391e4a2b2379a87cf2a7c60bf
chskela <chskela@gmail.com>
2022-08-27 11:28:08 +0300

    Added MainScreenUiState.kt

---
+++ app/src/main/java/com/chskela/pomodorotimer/presentation/ui/screens/main/MainScreenUiState.kt
@@ -0,0 +1,10 @@
+package com.chskela.pomodorotimer.presentation.ui.screens.main
+
+import com.chskela.pomodorotimer.domain.PomodoroState
+
+data class MainScreenUiState(
+    var pomodoroState: PomodoroState,
+    var isRunnable: Boolean,
+    var minutes: String,
+    var seconds: String
+)
